1e304a266e84e9f7b760a5fb34a548f0b1a9511f
alexandregaudencio <alexandre.gtpjr@gmail.com>
2020-07-30 22:28:15 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../../assets/img/LogoV.png';
+import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
+
+function Menu () {
+    return (
+        <nav className="Menu">
+            <a  href="/">
+                <img  className="Logo" src={Logo} alt="VeggieFlix logo"/>
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/"> 
+        Novo video
+            </Button>
+
+        </nav>
+    );
+}
+
+export default Menu;
+
+
