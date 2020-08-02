702711d7cd7a09b1c66705d5b10068d40d94de64
alexandregaudencio <alexandre.gtpjr@gmail.com>
2020-08-02 20:58:38 -0300

    Parte 2 de alterações

---
+++ src/pages/home/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Menu from '../../components/Menu';
+import styled from 'styled-components';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
+
+const AppWrapper = styled.div `
+  background: var(--grayDark);
+
+`;
+
+
+function Home() {
+  return (
+    <AppWrapper style={{background: "#141414"}} >
+       <Menu /> 
+
+      <bannerMain 
+          videoTitle={dadosIniciais.categorias[0].videos[0].titulo} 
+          url={dadosIniciais.categorias[0].videos.url} 
+          videoDescription={"O que é front-end? trabalhando na área os termos HTML, CSS e JS."}  />
+
+     <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+
+      <Carousel category={dadosIniciais.categorias[1]} />
+
+      <Carousel category={dadosIniciais.categorias[2]} />
+
+      <Carousel category={dadosIniciais.categorias[3]} />
+
+      <Carousel category={dadosIniciais.categorias[4]} />
+
+      <Carousel category={dadosIniciais.categorias[5]} />
+
+      <Footer />
+
+
+    </AppWrapper>
+  );
+}
+
+export default Home;
